e88a03f8175d2de7402c1dbb631771222bacc849
mlcollyer <mlcollyer@gmail.com>
2021-01-08 07:37:56 -0500

    simple error in lm.rrpp

--- R/lm.rrpp.r
+++ R/lm.rrpp.r
@@ -407,7 +407,8 @@ lm.rrpp <- function(f1, iter = 999, seed = NULL, int.first = FALSE,
   }
   
   trms <- attr(Terms, "term.labels")
-  k <- length(fits$full)
+  k <- 0
+  if(length(trms) > 0) k <- length(fits$full)
   id <- rownames(Y)
   ind <- perm.index(n, iter = iter, seed = seed)
   perms <- iter + 1
