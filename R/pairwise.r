73fc69b1ec282fd9332cad7383f1d12689b154c6
mlcollyer <mlcollyer@gmail.com>
2022-03-29 18:29:27 -0400

    pcov crossprod pairwise issue

--- R/pairwise.r
+++ R/pairwise.r
@@ -367,7 +367,7 @@ pairwise <- function(fit, fit.null = NULL, groups, covariate = NULL,
   
   if(gls) res <- fitf$LM$gls.residuals else res <- fitf$LM$residuals
   disp.args <- list(res = as.matrix(res), ind.i = NULL, x = model.matrix(~groups + 0))
-  if(gls) disp.args$x <- crossprod(fitf$LM$Pcov, disp.args$x)
+  if(gls) disp.args$x <- fitf$LM$Pcov %*% disp.args$x
   g.disp <- function(res, ind.i, x) {
     r <- res[ind.i,]
     if(NCOL(r) > 1) d <- apply(r, 1, function(x) sum(x^2)) else
